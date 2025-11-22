70ed0d2a9702c62cb399b1225c48e3b79e9fb4be
stackOverHeap <kanumera.riaz@gmail.com>
2025-11-22 19:06:52 +0100

    buffer len is now into the class

--- source code/include/radio.h
+++ source code/include/radio.h
@@ -14,6 +14,7 @@ class RadioManager
 private:
     RH_RF69 * m_radio; // singleton instance
     uint8_t m_buf[RH_RF69_MAX_MESSAGE_LEN]; // buffer for the radio message
+    uint8_t m_buf_len{sizeof(m_buf)};
     RadioRcvCb_t m_rcv_cb; // callback for the radio message reception
 
 public:
