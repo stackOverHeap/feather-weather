9fafcb6a23844469cc11eaa67b42889d3e03cb5f
stackOverHeap <kanumera.riaz@gmail.com>
2026-04-28 08:45:40 +0200

    add : event better power save with wtd wakeup

--- source code/src/module_main.cpp
+++ source code/src/module_main.cpp
@@ -16,6 +16,9 @@
 #define WKPIN 0 // ne555 out
 #define TRGPIN 1 // ne555 trig
 
+#define SLEEP_TIME_S 64
+#define SLEEP_CYCLE_COUNT (SLEEP_TIME_S / 8)
+
 #define MIN_BATTERY_VOLTAGE 3.5f
 
 RH_RF69 radio(RFM69_CS, RFM69_INT);
@@ -36,6 +39,8 @@ void wakeup_isr()
   digitalWrite(TRGPIN, HIGH);
 }
 
+ISR(WDT_vect) {}
+
 void setup() {
   Serial.begin(115200);
 
@@ -47,6 +52,8 @@ void setup() {
 
   // Init AHT20 sensor
   aht.begin();
+
+#ifdef NE555
   pinMode(WKPIN, INPUT_PULLUP); 
   // Configure INT2 (D0) to trigger on falling edge
   attachInterrupt(digitalPinToInterrupt(WKPIN), wakeup_isr, FALLING);
@@ -54,24 +61,28 @@ void setup() {
   pinMode(TRGPIN, OUTPUT);
   digitalWrite(TRGPIN, HIGH); // prepare for ne555 wakeup pulse
 
+  wdt_disable();
+#endif
+
   power_usart0_disable();
   power_timer1_disable();
   power_timer2_disable();
+  power_timer3_disable();
   power_usb_disable();
-  wdt_disable();
-  
 }
 
 void loop() {  
+#ifdef NE555
   EIMSK &= ~(1 << INT2);    // Disable INT2 interrupt
-
+#endif
   // Wake up sensor, get data
+  analogRead(VBATPIN); // dummy read
   update_vbat();
 
   sensors_event_t humidity, temp;
   aht.getEvent(&humidity, &temp);
 
-  paquet_data_strucutre payload 
+  paquet_data_structure payload 
   {
     .temperature = temp.temperature,
     .humidity = humidity.relative_humidity,
@@ -79,7 +90,7 @@ void loop() {
   };
 
   // Send data
-  radio.send(reinterpret_cast<uint8_t*>(&payload), sizeof(paquet_data_strucutre));
+  radio.send(reinterpret_cast<uint8_t*>(&payload), sizeof(paquet_data_structure));
   radio.waitPacketSent();
 
   // Put the radio into sleep mode
@@ -90,18 +101,46 @@ void loop() {
   power_spi_disable();
   power_timer0_disable();
 
+#ifdef NE555
   // Now enter deep sleep
   digitalWrite(TRGPIN, LOW);
   delayMicroseconds(10);
   digitalWrite(TRGPIN, HIGH);
-
   EIFR = bit(INTF2); // clear pending INT2
   EIMSK |= (1 << INT2);    // Enable INT2 interrupt
+#endif
+
   set_sleep_mode(SLEEP_MODE_PWR_DOWN);
 
+#ifdef NE555
   sleep_mode();
+#else
+  int cycle_count = 0;
 
-  // Re-enable ADC
+  sleep_enable();
+
+  cli();
+  wdt_reset();
+  WDTCSR |= (1 << WDCE) | (1 << WDE);
+  WDTCSR = (1 << WDIE) | (1 << WDP3) | (1 << WDP0); // interrupt only
+  sei();
+
+  while (cycle_count < SLEEP_CYCLE_COUNT)
+  {
+    sleep_cpu();
+    cycle_count++;
+  }
+
+  cli();
+  wdt_reset();
+  WDTCSR |= (1 << WDCE) | (1 << WDE);
+  WDTCSR = 0x00; // WDT fully off
+  sei();
+
+  sleep_disable();
+#endif
+
+  //  Re-enable ADC
   power_adc_enable();
   power_twi_enable();
   power_spi_enable();
